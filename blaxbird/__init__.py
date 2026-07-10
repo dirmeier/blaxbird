f940d9cde17a81d1b293b6bbe8eaef81281abdbb
Simon Dirmeier <simd23@protonmail.com>
2026-07-10 23:48:26 +0200

    feat: export get_ema_hook from blaxbird

--- blaxbird/__init__.py
+++ blaxbird/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.1.1"
 
 from blaxbird._src.checkpointer import get_default_checkpointer
+from blaxbird._src.hooks import get_ema_hook
 from blaxbird._src.trainer import train_fn
 
-__all__ = ["get_default_checkpointer", "train_fn"]
+__all__ = ["get_default_checkpointer", "get_ema_hook", "train_fn"]
